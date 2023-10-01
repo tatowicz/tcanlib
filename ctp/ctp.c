a2108543f6684ba0fb6ac785422fd5754602f838
Anthony Tatowicz Jr <ajtatowicz@gmail.com>
2023-10-01 16:24:16 -0500

    Update the cli and fix up rx

--- ctp/ctp.c
+++ ctp/ctp.c
@@ -79,24 +79,26 @@ int32_t ctp_receive(uint8_t* buffer, uint32_t buffer_size) {
     bool start_frame_received = false;
     uint32_t can_id;  
 
-    const uint32_t MAX_RETRIES = 1000;
-    uint32_t retry_count = 0;
+    //const uint32_t MAX_RETRIES = 1000;
+    //uint32_t retry_count = 0;
 
     while (received_length < expected_total_length || !start_frame_received) {
         if (!receive_ctp_message(&can_id, can_data, &length)) {
-            retry_count++;
-            if (retry_count >= MAX_RETRIES) {
-                return -1;  // Return error if max retry count is reached
-            }
+            //retry_count++;
+            // if (retry_count >= MAX_RETRIES) {
+            //      return -1;  // Return error if max retry count is reached
+            // }
             continue;  // Keep trying until we get a message or reach max retries
         }
 
         // Reset retry count on successful reception
-        retry_count = 0;
+        //retry_count = 0;
 
         if (!start_frame_received && can_data[0] == CTP_START_FRAME) {
             expected_total_length = can_data[1];
+
             if (expected_total_length > buffer_size) {
+                printf("Buffer provided is not enough: expected_total_length=%u, buffer_size=%u\n", expected_total_length, buffer_size);
                 return -1;  // Error: buffer provided is not enough
             }
 
@@ -107,7 +109,7 @@ int32_t ctp_receive(uint8_t* buffer, uint32_t buffer_size) {
             start_frame_received = true;
 
             if (expected_total_length == start_frame_length) {
-                return received_length;  // Successfully received the full frame
+                return received_length;
             }
             
             continue;
@@ -117,9 +119,11 @@ int32_t ctp_receive(uint8_t* buffer, uint32_t buffer_size) {
             switch (can_data[0]) {
                 case CTP_CONSECUTIVE_FRAME:
                     if (can_data[1] != expected_sequence_number) {
+                        printf("Expected sequence number %u but received %u\n", expected_sequence_number, can_data[1]);
                         return -1;  // Error: sequence mismatch
                     }
                     if ((received_length + CTP_CONSECUTIVE_DATA_LENGTH) > buffer_size) {
+                        printf("Buffer overflow: received_length=%u, buffer_size=%u\n", received_length, buffer_size);
                         return -1;  // Error: buffer overflow
                     }
                     memcpy(&buffer[received_length], &can_data[2], CTP_CONSECUTIVE_DATA_LENGTH);
@@ -129,6 +133,7 @@ int32_t ctp_receive(uint8_t* buffer, uint32_t buffer_size) {
 
                 case CTP_END_FRAME:
                     if ((received_length + CTP_END_DATA_LENGTH) > buffer_size) {
+                        printf("Buffer overflow: received_length=%u, buffer_size=%u\n", received_length, buffer_size);
                         return -1;  // Error: buffer overflow
                     }
                     memcpy(&buffer[received_length], &can_data[1], CTP_END_DATA_LENGTH);
--- ctp/ctp_cli.c
+++ ctp/ctp_cli.c
@@ -8,10 +8,11 @@
 #include "PCBUSB.h"
 
 
+// Global pcan channel for processes to use
+uint32_t pcan_channel = PCAN_USBBUS1;
 
-#define PCAN_CHANNEL PCAN_USBBUS1
 
-
+// Driver function for PCAN-USB MAC, used by the CTP to send frames to the CAN bus
 bool send_ctp_message(uint32_t id, const uint8_t *data, uint8_t length) {
     TPCANStatus status;
     TPCANMsg message;
@@ -21,7 +22,7 @@ bool send_ctp_message(uint32_t id, const uint8_t *data, uint8_t length) {
     message.MSGTYPE = PCAN_MESSAGE_STANDARD;
     memcpy(message.DATA, data, message.LEN);
 
-    status = CAN_Write(PCAN_CHANNEL, &message);
+    status = CAN_Write(pcan_channel, &message);
 
     if (status != PCAN_ERROR_OK) {
         printf("Failed to send CAN message: 0x%x\n", status);
@@ -31,28 +32,22 @@ bool send_ctp_message(uint32_t id, const uint8_t *data, uint8_t length) {
     return true;
 }
 
+// Driver function for PCAN-USB MAC, used by the CTP to get frames from the CAN bus
 bool receive_ctp_message(uint32_t *id, uint8_t *data, uint8_t *length) {
     TPCANMsg message;
     TPCANStatus status;
 
-    status = CAN_Read(PCAN_CHANNEL, &message, NULL);
+    status = CAN_Read(pcan_channel, &message, NULL);
 
-    if (status == PCAN_ERROR_OK) {
-        if (!(message.MSGTYPE & PCAN_MESSAGE_STATUS)) {
-            printf("  - R ID:%4x LEN:%1x DATA:%02x %02x %02x %02x %02x %02x %02x %02x\n",
-                (int) message.ID, (int) message.LEN, (int) message.DATA[0],
-                (int) message.DATA[1], (int) message.DATA[2],
-                (int) message.DATA[3], (int) message.DATA[4],
-                (int) message.DATA[5], (int) message.DATA[6],
-                (int) message.DATA[7]);
-        }
+    if (status == PCAN_ERROR_OK) {        
+        *id = message.ID;
+        *length = message.LEN;
+        memcpy(data, message.DATA, message.LEN);
     } 
     else if (status == PCAN_ERROR_QRCVEMPTY) {
-        printf("Receive queue is empty.\n");
         return false;
     } 
     else {
-        printf("Error 0x%x\n", status);
         return false;
     }
 
@@ -69,45 +64,165 @@ void send_data(uint32_t id, const char* data) {
 
 void receive_data() {
     uint8_t buffer[512];  // Adjust the buffer size as needed.
-    int32_t received_bytes = ctp_receive(buffer, sizeof(buffer));
-    
-    if (received_bytes == -1) {
-        printf("Error receiving data.\n");
-    } else {
-        printf("Received data: %.*s\n", received_bytes, buffer);
+
+    while (1) {
+        int32_t received_bytes = ctp_receive(buffer, sizeof(buffer));
+        
+        if (received_bytes > 0) {
+            printf("Received data: %.*s\n", received_bytes, buffer);
+            
+            for (uint32_t i = 0; i < received_bytes; i++) {
+                printf("%02x ", buffer[i]);
+            }
+            printf("\n");
+        } 
     }
 }
 
-int main(int argc, char *argv[]) {
-    TPCANStatus status;
+uint32_t init_can(uint32_t channel, uint32_t baud_rate) {
+    uint32_t status = false;
 
-    if (argc < 2) {
-        printf("Send data over CAN using CTP protocol.\n");
-        printf("Usage: %s <send> <CAN_ID> [data]\n", argv[0]);
-        printf("Usage: %s <dump>\n", argv[0]);
-        printf("Example: %s send 0x123 \"Hello World!\"\n", argv[0]);
+    uint32_t channel_map[] = {
+        PCAN_USBBUS1,
+        PCAN_USBBUS2,
+        PCAN_USBBUS3,
+        PCAN_USBBUS4,
+        PCAN_USBBUS5,
+        PCAN_USBBUS6,
+        PCAN_USBBUS7,
+        PCAN_USBBUS8
+    };
+
+    if (channel > 7) {
+        printf("Invalid channel: %d\n", channel);
         return 1;
     }
 
-    if (strcmp(argv[1], "send") == 0) {
-        if (argc < 4) {
-            printf("Please provide CAN ID and data to send.\n");
-            return 1;
-        }
+    switch(baud_rate) {
+        case 1000:
+            baud_rate = PCAN_BAUD_1M;
+            break;
 
-        status = CAN_Initialize(PCAN_CHANNEL, PCAN_BAUD_250K, 0, 0, 0);
-        printf("Initialize CAN, Status = 0x%x\n", status);
-        if (status != PCAN_ERROR_OK) {
+        case 500:
+            baud_rate = PCAN_BAUD_500K;
+            break;
+
+        case 250:
+            baud_rate = PCAN_BAUD_250K;
+            break;
+
+        default:
+            printf("Invalid baud rate: %d\n", baud_rate);
+            return 1;
+    }
+
+    pcan_channel = channel_map[channel];
+
+    status = CAN_Initialize(pcan_channel, baud_rate, 0, 0, 0);
+    printf("Initialize CAN, Status = 0x%x\n", status);
+
+    if (status != PCAN_ERROR_OK) {
             printf("Failed to initialize CAN\n");
             return 1;
-        }
+    }
 
-        uint32_t can_id = (uint32_t)strtoul(argv[2], NULL, 0);
-        send_data(can_id, argv[3]);
-    } else if (strcmp(argv[1], "dump") == 0) {
-        receive_data();
-    } else {
-        printf("Invalid command. Use 'send' or 'dump'.\n");
+    return 0;
+}
+
+void print_help() {
+    printf("Usage: cli [OPTIONS] COMMAND [ARGS]\n\n");
+
+    printf("Options:\n");
+    printf("  -i, --interface <num>   Specify the interface number.\n");
+    printf("  -h, --help              Show this help message and exit.\n\n");
+
+    printf("Commands:\n");
+    printf("  send                    Send a command.\n");
+    printf("    --id <num>            Specify the ID for the send command.\n");
+    printf("    --data <string>       Specify the data for the send command.\n");
+    printf("  dump                    Dump the data.\n\n");
+
+    printf("Args for both send and dump:\n");
+    printf("    --baud <num>          Specify the baud rate.\n\n");
+
+    printf("Examples:\n");
+    printf("  cli -i 1 send --id 123 --data \"hello\" --baud 250\n");
+    printf("  cli -i 1 dump --baud 250\n");
+}
+
+int main(int argc, char *argv[]) {
+    int interface = -1;
+    int baud_rate = -1;
+    int id = -1;
+    char *data = NULL;
+
+    if (argc < 2) {
+        print_help();
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            print_help();
+            return 0;
+        } 
+        else if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--interface") == 0) {
+            if (++i < argc) {
+                interface = atoi(argv[i]);
+            }
+        } 
+        else if (strcmp(argv[i], "send") == 0) {
+            i++;
+            while (i < argc) {
+                if (strcmp(argv[i], "--id") == 0) {
+                    if (++i < argc) {
+                        id = atoi(argv[i]);
+                    }
+                } else if (strcmp(argv[i], "--data") == 0) {
+                    if (++i < argc) {
+                        data = argv[i];
+                    }
+                } else if (strcmp(argv[i], "--baud") == 0) {
+                    if (++i < argc) {
+                        baud_rate = atoi(argv[i]);
+                    }
+                } else {
+                    break;
+                }
+                i++;
+            }
+            if (id != -1 && data != NULL) {
+                uint32_t status = init_can(interface, baud_rate);
+
+                if (status != 0) {
+                    printf("Failed to initialize CAN\n");
+                    return 1;
+                }
+
+                printf("Send command with id=%d, data=%s, baud_rate=%dk\n", id, data, baud_rate);
+                send_data(id, data);
+            } 
+            else {
+                printf("Send command is missing required arguments.\n");
+            }
+        } 
+        else if (strcmp(argv[i], "dump") == 0) {
+            if (++i < argc && (strcmp(argv[i], "--baud") == 0)) {
+                if (++i < argc) {
+                    baud_rate = atoi(argv[i]);
+                }
+            }
+
+            uint32_t status = init_can(interface, baud_rate);
+
+            if (status != 0) {
+                printf("Failed to initialize CAN\n");
+                return 1;
+            }
+
+            printf("Dump command with baud_rate=%dk\n", baud_rate);   
+            receive_data();
+        }
     }
 
     return 0;
